80dc3b86bf69dcbab59015761ac33787888de3b6
mpbugatti <62277479+mpbugatti@users.noreply.github.com>
2021-05-18 10:44:58 -0700

    Added code for Gate

--- src/gate.py
+++ src/gate.py
@@ -1,14 +1,15 @@
-﻿# from adafruit_servokit import ServoKit
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
 
 class gateSubsystem:
     def __init__ (self):
         pass
 
     def open (self):
-        pass
+        kit.servo[0].angle = 25
 
     def close (self):
-        pass
+        kit.servo[0].angle = 90
 
     def moveServo (self, angle):
-        pass
+        kit.servo[0].angle = angle
